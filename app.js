30ebed93614ab14f2596151e0367158a9800791f
iwelnemr <70034333+iwelnemr@users.noreply.github.com>
2020-09-19 14:49:58 +0200

    Allow for storing data retrieved from server in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* Global Variables */
+const projectData = {};
 
 // Create a new date instance dynamically with JS
 
@@ -18,10 +19,10 @@ function action() {
   .then(function (data) {
     console.log(data);
     console.log("success");
-    sendData('/add', {temperature: data.main.temp, date:data.dt, userResponse:userResponse});
+    sendData({temperature: data.main.temp, date:data.dt, userResponse:userResponse});
   })
 
-  .then (setTimeout(changeUI, 4000))
+  .then (setTimeout(changeUI(projectData), 4000))
 }
 
 
@@ -43,42 +44,26 @@ catch(error) {
 }
 
 /**** sendData ****/
-const sendData = async (url='', data={}) => {
+const sendData = (data = {}) => {
 console.log(data);
-const res = await fetch(url, {
-  method: 'POST',
-  credentials: 'same-origin',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-});
 
-try {
-  const returnedData = await res.json();
-  console.log(returnedData);
-  return returnedData;
-} catch (error) {
-  console.log("error", error)
-}
+projectData.temperature = req.body.temperature;
+projectData.date = req.body.date;
+projectData.userResponse = req.body.userResponse;
 
 };
 
 /**** changeUI ****/
-const changeUI = async () => {
-  const req = await fetch ('/add');
-  try {
-    const all = await req.json();
-    console.log(all);
+const changeUI = (data = {}) => {
+  
+    console.log(data);
 
-    let d = new Date(all.date * 1000);
+    let d = new Date(data.date * 1000);
     let newDate = (d.getMonth()+1)+'.'+ d.getDate()+'.'+ d.getFullYear();
 
     document.getElementById('date').innerHTML = newDate;
-    document.getElementById('temp').innerHTML = all.temperature;
-    document.getElementById('content').innerHTML = all.userResponse;
+    document.getElementById('temp').innerHTML = data.temperature;
+    document.getElementById('content').innerHTML = data.userResponse;
     return all;
-  } catch(error) {
-    console.log("error", error);
-  }
+
 }
